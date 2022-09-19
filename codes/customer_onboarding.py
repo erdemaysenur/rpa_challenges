2ec95d5fb22931b01cdefd27e44b870de028506d
Ayse Nur Erdem <56694906+erdemaysenur@users.noreply.github.com>
2022-09-19 16:17:31 +0300

    Remove unused imports

--- codes/customer_onboarding.py
+++ codes/customer_onboarding.py
@@ -1,9 +1,5 @@
 from selenium import webdriver
-#from selenium.webdriver.support.ui import WebDriverWait
-#from selenium.webdriver.support import expected_conditions as EC
-#from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-#from selenium.webdriver.support.ui import Select
 import pandas as pd
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
